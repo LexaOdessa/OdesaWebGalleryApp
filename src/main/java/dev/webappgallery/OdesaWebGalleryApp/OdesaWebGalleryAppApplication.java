e04777e5f8ce83f1b691421658045c0e55cc773f
Alex Violin <60292383+LexaOdessa@users.noreply.github.com>
2024-09-20 01:35:01 +0300

    TestCommit for this project

---
+++ src/main/java/dev/webappgallery/OdesaWebGalleryApp/OdesaWebGalleryAppApplication.java
@@ -0,0 +1,13 @@
+package dev.webappgallery.OdesaWebGalleryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdesaWebGalleryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdesaWebGalleryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/webappgallery/OdesaWebGalleryApp/OdesaWebGalleryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.webappgallery.OdesaWebGalleryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdesaWebGalleryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
